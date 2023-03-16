8ffb05cd2ca1d496dcfb72abe987758de7c8f66f
Archanatshinod <116487133+Archanatshinod@users.noreply.github.com>
2023-03-16 08:41:39 -0700

    Add files via upload

---
+++ sumofsigits.java
@@ -0,0 +1,15 @@
+class Sum   
+{  
+public static void main(String args[])  
+{  
+int num, digit, sum = 0;  
+num=1234;
+while(number > 0)  
+{  
+digit = number % 10;  
+sum = sum + digit;  
+number = number / 10;  
+} 
+System.out.println("Sum of Digits: "+sum);  
+}  
+}
